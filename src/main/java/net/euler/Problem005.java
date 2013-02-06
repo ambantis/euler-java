03663f63f0724254fdf3c79bf9669ce6df067363
Alexandros Bantis <ambantis@gmail.com>
2013-02-06 08:17:24 -0800

    optimized  prob #5 to iterate through ceiling instead of n

--- src/main/java/net/euler/Problem005.java
+++ src/main/java/net/euler/Problem005.java
@@ -1,7 +1,5 @@
 package net.euler;
 
-import org.javatuples.Pair;
-
 /**
  * Smallest Multiple
  *
@@ -33,6 +31,7 @@ public class Problem005 {
     return number;
   }
 
+
   private static int intPow(int base, int exponent) {
     int value = 1;
     for (int i = 0; i < exponent; i++)
@@ -40,28 +39,38 @@ public class Problem005 {
     return value;
   }
 
-  public static int[] primesTo(int n) {
+  /**
+   * primesTo returns all primes numbers up to n using trial by division algorithm
+   *
+   *
+   * @param n designates primes should be in the range 2 ... n
+   * @return int[] a sieve of all prime factors (0=CROSSED_OUT, 1=NOT_CROSSED_OUT)
+   */
+  private static int[] primesTo(int n) {
     int ceiling = (int) Math.sqrt(n * 1.0) + 1;
-    int[] factors = new int[n+1];
+    int[] sieve = new int[n+1];
 
     // set default values
-    for (int i = 0; i <= n; i++)
-      factors[i] = NOT_CROSSED_OUT;
+    for (int i = 2; i <= n; i++)
+      sieve[i] = NOT_CROSSED_OUT;
 
     // cross out sieve values
-    factors[0] = CROSSED_OUT;
-    factors[1] = CROSSED_OUT;
-    for (int i = 2; i < ceiling; i++)
+    for (int i = 2; i <= ceiling; i++)
       for (int j = 2; i*j <= n; j++)
-        factors[i*j] = CROSSED_OUT;
-    return factors;
+        sieve[i*j] = CROSSED_OUT;
+    return sieve;
   }
 
 
-  // elegant algorithm
-  public static int[] getPrimeFactors(int n) {
+  /**
+   * getPrimeExp returns a prime factorization array
+   * @param n the number subject to prime factorization
+   * @return int[] an array of exponents for prime factors of n
+   *               e.g., 8 => (0^0, 1^0, 2^3, 3^0, 4^0, 5^0, 6^0, 7^0, 8^0)
+   */
+  private static int[] getPrimeExp(int n) {
     int[] factor = primesTo(n);
-    int[] power = new int[n+1];
+    int[] primePowAll = new int[n+1];
 
     // set prime_factor_exponent for all factor/exponent pairs
     for (int i = 2; i <= n; i++) {
@@ -69,7 +78,7 @@ public class Problem005 {
         while (true) {
           if (n % i == 0) {
           n /= i;
-          power[i] += 1;
+          primePowAll[i] += 1;
           } else {
             break;
           }
@@ -77,19 +86,19 @@ public class Problem005 {
       }
     }
 
-    return power;
+    return primePowAll;
   }
 
   public static int findSmallestMultipleElegant(int n) {
-    int[] power = new int[n+1];
+    int[] gcfAll = new int[n+1];
 
     // populate greatest common factor arrays
-    int[] primeFactorExponents = null;
+    int[] gcfThis = null;
     for (int i = 2; i <= n; i++) {
-      primeFactorExponents = getPrimeFactors(i);
+      gcfThis = getPrimeExp(i);
       for (int j = 2; j <= i; j++) {
-        if (primeFactorExponents[j] > 0 && primeFactorExponents[j] > power[j]) {
-          power[j] = primeFactorExponents[j];
+        if (gcfThis[j] > 0 && gcfThis[j] > gcfAll[j]) {
+          gcfAll[j] = gcfThis[j];
         }
       }
     }
@@ -97,8 +106,8 @@ public class Problem005 {
     // multiply out gcf arrays
     int value = 1;
     for (int i = 2; i <= n; i++) {
-      if (power[i] > 0)
-        value *= intPow(i, power[i]);
+      if (gcfAll[i] > 0)
+        value *= intPow(i, gcfAll[i]);
     }
     return value;
   }
