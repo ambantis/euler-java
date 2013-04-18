45dab0846a68615b2e45969b9f0535644439ac88
Alexandros Bantis <ambantis@gmail.com>
2013-04-18 09:58:42 -0700

    begin problem 29

---
+++ src/test/java/net/euler/Problem029Test.java
@@ -0,0 +1,23 @@
+package net.euler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * User: Alexandros Bantis
+ * Date: 4/18/13
+ * Time: 9:55 AM
+ */
+@RunWith(JUnit4.class)
+public class Problem029Test {
+
+  @Test
+  public void testDistinctPowers() {
+    int expected = 0;
+    int actual = 1;
+    assertEquals("failure, test not set up yet", expected, actual);
+  }
+
+}
